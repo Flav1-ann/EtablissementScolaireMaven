0a0c59a195049b38fa6048e1d6e9670857284aa0
flav1-ann <Moitoi28>
2021-04-01 09:51:51 +0200

    update pom for juint 5

--- EtablissementScolaire-service/src/test/java/eu/ensup/etablissementscolaire/SpyEtudiantServiceTest.java
+++ EtablissementScolaire-service/src/test/java/eu/ensup/etablissementscolaire/SpyEtudiantServiceTest.java
@@ -3,27 +3,27 @@ package eu.ensup.etablissementscolaire;
 import eu.ensup.etablissementscolaire.exception.ServiceException;
 import eu.ensup.etablissementscolaire.exception.etudiantExceptions.GetEtudiantServiceException;
 import eu.ensup.etablissementscolaire.exceptions.DaoException;
-import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.sql.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class SpyEtudiantServiceTest {
 
     @Spy
     IEtudiantService service;
 
     @Test
-    public void testGetOneUtilisateur() throws DaoException, ServiceException {
+    public void testGetOneUtilisateur() throws  ServiceException {
 
         //Subbing
         Mockito.doReturn(new Etudiant(2,"name","Annaix"+Math.random()+"@gmail.com","24 b"
